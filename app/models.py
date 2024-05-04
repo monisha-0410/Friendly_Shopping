51ce2d51c3edcc969294f35427c8a350c63c6b28
monisha0410 <monishashalini2002@gmail.com>
2024-05-05 00:55:25 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import User
+# Create your models here.
+class Profile(models.Model):
+    username=models.OneToOneField(User,on_delete=models.CASCADE)
+    address=models.CharField(max_length=50)
+    profile_picture=models.ImageField()
+
+    def __str__(self):
+        return self.username
